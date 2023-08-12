232276911dca2c8007c4b99809064e632150af44
SafaeQ <safaedani009@gmail.com>
2023-08-12 23:44:17 +0100

    📝 update

--- src/Sections/Main.jsx
+++ src/Sections/Main.jsx
@@ -23,9 +23,35 @@ const Main = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-12 grid-rows-4 gap-4 ">
-          <div className="col-span-11 row-span-4 col-start-2 row-start-1 bg-white shadow-sm w-[80%] ml-[57px] ">
+        <div className="grid grid-cols-12 grid-rows-4 gap-6 w-[90%] justify-end ">
+          <div className="col-span-6 row-span-4 col-start-2 bg-white rounded-[5px] shadow">
             1
+          </div>
+          <div className="col-span-3 row-span-4 col-start-8  bg-white rounded-[5px] shadow p-4 flex flex-col gap-5">
+            <span className="text-zinc-600 text-[13px] font-semibold flex justify-start">
+              Image
+            </span>
+            <div className="grid grid-cols-1 space-y-2">
+              <label className="flex flex-col rounded-lg border-2 border-dashed group text-center">
+                <div className=" text-center flex flex-col items-center justify-center  ">
+                  <p className="pointer-none text-gray-500 ">
+                    <span className="text-sm">Drag and drop</span> files here{" "}
+                    <br /> or
+                    <a href="" id="" className="text-blue-600 hover:underline">
+                      select a file
+                    </a>
+                    from your computer
+                  </p>
+                </div>
+                <input type="file" className="hidden" />
+              </label>
+            </div>
+            <p className="text-zinc-400 text-[10px] font-semibold">
+              *Only JPG, PNG files are allowed. Image must be less than 2 MB
+            </p>
+          </div>
+          <div className="col-span-2 row-span-4 col-start-11 bg-white rounded-[5px] shadow">
+            4
           </div>
         </div>
         <div>09</div>
