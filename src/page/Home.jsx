9185da7a31ea78eacce195747a70d8323a72dea4
SafaeQ <safaedani009@gmail.com>
2023-08-12 17:22:43 +0100

    📝  update

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "../Sections/NavBar";
+import Main from "../Sections/Main";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <NavBar />
-      <h1>home</h1>
-    </div>
+      <Main />
+    </>
   );
 };
 
