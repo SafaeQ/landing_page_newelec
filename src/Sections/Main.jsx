526b4eddd5eda587a772a4b136f3841b2a7ad47b
SafaeQ <safaedani009@gmail.com>
2023-08-14 12:44:04 +0100

    🔥 add carousel

--- src/Sections/Main.jsx
+++ src/Sections/Main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { IoMdCloudUpload } from "react-icons/io";
-// import { AiFillCheckSquare } from "react-icons/ai";
 import CustomSelect from "../Components/CustomSelect";
 import Section2 from "./Section2";
-
-import {
-  Carousel,
-  initTE,
-} from "tw-elements";
-
-initTE({ Carousel });
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Main = () => {
   const selectOptions = [{ icon: "", label: "Safty" }];
@@ -285,106 +279,15 @@ const Main = () => {
             </button>
           </div>
           <div className="w-full rounded-[5px] p-4 flex flex-row gap-4 mt-4">
-            {/* <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div>
-            <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div>
-            <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div>
-            <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div>
-            <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div>
-            <div class="w-[50%] h-[156px] bg-white rounded-[5px] shadow">h</div> */}
-
-            <div
-              id="carouselExampleControls"
-              class="relative"
-              data-te-carousel-init
-              data-te-ride="carousel"
-            >
-              <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-                <div
-                  class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item
-                  data-te-carousel-active
-                >
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-                    class="block w-full"
-                    alt="Wild Landscape"
-                  />
-                </div>
-                <div
-                  class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item
-                >
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-                    class="block w-full"
-                    alt="Camera"
-                  />
-                </div>
-                <div
-                  class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item
-                >
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-                    class="block w-full"
-                    alt="Exotic Fruits"
-                  />
-                </div>
-              </div>
-
-              <button
-                class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-                type="button"
-                data-te-target="#carouselExampleControls"
-                data-te-slide="prev"
-              >
-                <span class="inline-block h-8 w-8">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-6 w-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M15.75 19.5L8.25 12l7.5-7.5"
-                    />
-                  </svg>
-                </span>
-                <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                  Previous
-                </span>
-              </button>
-              <button
-                class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-                type="button"
-                data-te-target="#carouselExampleControls"
-                data-te-slide="next"
-              >
-                <span class="inline-block h-8 w-8">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-6 w-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                    />
-                  </svg>
-                </span>
-                <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                  Next
-                </span>
-              </button>
-            </div>
+            <Carousel showThumbs={false} showStatus={false} showIndicators={false} emulateTouch infiniteLoop>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h1</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h2</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h3</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h4</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h5</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h6</div>
+              <div className="h-[156px] bg-white rounded-[5px] shadow">h</div>
+            </Carousel>
           </div>
         </div>
       </div>
